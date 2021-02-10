d1bfa70b05287c5ac5f1ee2a760b5485cccf7a34
turpp <white.tripp@icloud.com>
2021-02-10 17:38:20 -0600

    commit tree fb9c5f0325a9de79b76115fc4d8a9891aedab11f

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,22 @@
 // Code SimpleComponent Here
+import React from 'react'
+
+export default class SimpleComponent extends React.Component{
+
+    state={mood: 'happy'}
+    handleClick=()=> {
+        if(this.state.mood === 'happy'){
+            this.setState({
+                mood: 'sad'
+            })
+        }else {
+            this.setState({
+                mood: 'happy'
+            })
+        }
+      }
+
+    render(){
+        return <div onClick={this.handleClick}>{this.state.mood}</div>
+    }
+}
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={()=>console.log('I am so happy the console knows')} />
   </div>,
   document.getElementById('root')
 );
